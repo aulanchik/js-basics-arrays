9e736624ea4d2aa554f3bb0070542466b1415fbc
Artyom Ulanchik <artyom.u@outlook.com>
2024-11-09 17:00:20 +0000

    added include example

---
+++ scripts/includes.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Array.prototype.includes()
+// The includes() method determines whether an array includes a certain element,
+// returning true or false as appropriate.
+
+const fruits = ['apple', 'banana', 'orange', 'kiwi', 'pineapple'];
+
+console.log('Array.prototype.includes():');
+console.log('Words: ', fruits);
+console.log('Includes "apple": ', fruits.includes('apple'));
+console.log('Includes "avocado": ', fruits.includes('avocado'));
