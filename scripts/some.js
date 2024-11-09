8dc8298f776b1cb56a1868845e1077c7b743c129
Artyom Ulanchik <artyom.u@outlook.com>
2024-11-09 16:17:01 +0000

    some example added

---
+++ scripts/some.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// Array.prototype.some()
+// The some() method tests whether at least one element in the array
+// passes the test implemented by the provided function.
+
+const someNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const divisibleByThree = someNumbers.filter((number) => number % 3 === 0);
+const hasDivisibleByThree = someNumbers.some((number) => number % 3 === 0);
+
+console.log('Array.prototype.some():');
+console.table({ 'All numbers': someNumbers });
+console.log('Divisible by 3:', divisibleByThree);
+console.log('Has divisible by 3:', hasDivisibleByThree);
