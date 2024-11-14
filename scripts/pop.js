42b87a4f8dc19bb49ec7aaa2cdaef745b4cc09bc
Artyom Ulanchik <artyom.u@outlook.com>
2024-11-14 19:47:53 +0000

    added pop example

---
+++ scripts/pop.js
@@ -0,0 +1,9 @@
+// Array.prototype.pop()
+// The pop() method removes the last element from an array and returns that element.
+// Please note that this method DOES change array size.
+
+const alphabet = ['a', 'b', 'c', 'd'];
+console.log('Array.prototype.pop():');
+console.log('Initial Alphabet: ', alphabet);
+alphabet.pop();
+console.log('After using pop(): ', alphabet);
